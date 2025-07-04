34ce353748dfff10cc1f26b14555a72379c2f945
nanahaya770 <asyaiadam@mail.ru>
2025-07-04 16:03:02 +0300

    удалили время ожидания

--- 07_lesson/test_01_calc_page_obj.py
+++ 07_lesson/test_01_calc_page_obj.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from pages.CalculatorPage import CalculatorPage
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 
 @pytest.fixture
@@ -21,8 +20,5 @@ def test_calculator(driver):
     calculator_page = CalculatorPage(driver)
     calculator_page.open()
     calculator_page.set_delay()
-    time.sleep(3)
     calculator_page.do_calc()
-    time.sleep(3)
     assert calculator_page.expected_result()
-    time.sleep(3)
