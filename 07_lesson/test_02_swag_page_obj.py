f560cf253fea1b3581fdc0fcf4308debd312fbcb
nanahaya770 <asyaiadam@mail.ru>
2025-07-06 15:44:42 +0300

    изменили браузер, удалили time sleep

--- 07_lesson/test_02_swag_page_obj.py
+++ 07_lesson/test_02_swag_page_obj.py
@@ -5,17 +5,14 @@ from pages.MainPage import MainPage
 from pages.CartPage import CartPage
 from pages.OrderPage import OrderPage
 from pages.TotalPage import TotalPage
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    driver.implicitly_wait(3)
-    # driver.maximize_window()
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver.implicitly_wait(20)
     yield driver
     print(driver)
     driver.quit()
@@ -25,29 +22,18 @@ def test_swag_site(driver):
     auth_page = AuthPage(driver)
     auth_page.open()
     auth_page.fill_filds()
-    time.sleep(3)
     auth_page.submit_form()
-    time.sleep(3)
 
     main_page = MainPage(driver)
     main_page.add_to_cart()
-    time.sleep(3)
     main_page.go_to_cart()
-    time.sleep(3)
 
     cart_page = CartPage(driver)
-    cart_page.chekout_click()
-    time.sleep(3)
+    cart_page.checkout()
 
     order_page = OrderPage(driver)
     order_page.fill_filds()
-    time.sleep(3)
     order_page.continue_click()
-    time.sleep(3)
 
     total_page = TotalPage(driver)
     total_page.total_result()
-
-    # driver.close()
-
-    # assert total == $58.29
