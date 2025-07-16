87d607d6e0788eda937cdc21eaf0ff03f7aae44b
nanahaya770 <asyaiadam@mail.ru>
2025-07-16 22:34:32 +0300

    запросы к БД

--- 09_lesson/test_select.py
+++ 09_lesson/test_select.py
@@ -1,12 +1,62 @@
-# db_connection_string = (postgresql://qa:skyqa@localhost:5432/x_clients)
 from sqlalchemy import create_engine, text
 
-try:
+
+def test_insert():
     engine = create_engine("postgresql://postgres:770@localhost:5432/postgres")
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1"))
-        print("✅ Соединение с БД успешно!", list(result))
-except Exception as e:
-    print("❌ Ошибка подключения:", e)
+        sql = "INSERT INTO subject (subject_id, subject_title" \
+            ") VALUES (21, 'Geometry')"  # строка с sql запросом
+        connection.execute(text(sql))  # выполнение срокового sql запроса к БД
+
+        sql = "select * from subject where subject_id = 21"
+        result = connection.execute(text(sql))
+
+        subject_id, subject_title = list(result)[0]
+        assert subject_id == 21
+        assert subject_title == 'Geometry'
+
+        sql = "DELETE FROM subject WHERE subject_id = 21"
+        connection.execute(text(sql))
 
 
+def test_update():
+    # подключение к БД
+    engine = create_engine("postgresql://postgres:770@localhost:5432/postgres")
+    with engine.connect() as connection:
+        # update изменить
+        sql = "UPDATE subject SET subject_title='Physics' WHERE subject_id=5"
+        connection.execute(text(sql))
+
+        # select
+        sql = "select * from subject where subject_id = 5"
+        result = connection.execute(text(sql))
+
+        # assert
+        subject_id, subject_title = list(result)[0]
+        assert subject_id == 5
+        assert subject_title == 'Physics'
+
+        # update вернуть обратно
+        sql = "UPDATE subject SET subject_title='Chess' WHERE subject_id=5"
+        connection.execute(text(sql))
+
+
+def test_delete():
+    # delete существующую строку
+    engine = create_engine("postgresql://postgres:770@localhost:5432/postgres")
+    with engine.connect() as connection:
+        # update изменить
+        sql = "DELETE FROM subject WHERE subject_id = 1"
+        connection.execute(text(sql))
+
+        # select
+        sql = "select * from subject where subject_id = 1"
+        result = connection.execute(text(sql))
+
+        # assert
+        assert list(result) == []
+
+        # insert вернуть удаленную строку
+        sql = "INSERT INTO subject (" \
+            "subject_id, subject_title) VALUES (1, 'English')"
+        connection.execute(text(sql))
